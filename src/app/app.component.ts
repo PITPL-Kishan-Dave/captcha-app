c8049afffff0671ee370c13a279ff584e589b8e9
Kishan Dave <kishandave99@gmaill.com>
2021-10-01 11:50:25 +0530

    captcha demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgRecaptcha3Service } from 'ng-recaptcha3';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'captcha-app';
+  myForm: FormGroup;
+  formData: any;
+  private siteKey = "6LedAJEUAAAAAPttxeFNp6ZtAvKGI8D9gESE-hl3";
+
+  constructor(
+    private fb: FormBuilder,
+    private recaptcha3: NgRecaptcha3Service
+  ) {
+    this.myForm = this.fb.group({
+      name: [null],
+    });
+  }
+
+  ngOnInit(){
+   
+
+    this.recaptcha3.init(this.siteKey);
+  }
+  
+  onSubmit() {
+    this.recaptcha3.getToken().then(
+      token => {
+        console.log(token)
+        console.log(this.formData);
+        // Handle saving form data
+        // this.myHttpService.login(this.formData).subscribe(response => {
+        //
+        // });
+      },
+      error => {
+        // handle error here
+        console.log(error);
+      }
+    );
+  }
 }
